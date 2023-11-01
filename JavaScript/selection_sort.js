d47f297ab25351867957bea5baa488f21af32729
Matheus Andrade <matheusvncs160@gmail.com>
2023-10-31 21:25:59 -0300

    selection_sort.js

---
+++ JavaScript/selection_sort.js
@@ -0,0 +1,18 @@
+function selectionSort(arr) {
+    const n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        }
+    }
+}
+
+const numbers = [64, 25, 12, 22, 11];
+selectionSort(numbers);
+console.log(numbers); // [11, 12, 22, 25, 64]
